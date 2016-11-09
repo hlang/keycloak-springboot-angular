67abcec7c384bc14ddc79f50dceb8087c1b825be
hartmut <hartmut.lang@gmail.com>
2016-11-09 22:21:09 +0100

    working with login, sending rest to backend

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,17 +1,57 @@
 var secDemoApp = angular.module('secDemoApp', []);
 
-secDemoApp.controller('secController', function ($scope, $http) {
+secDemoApp.controller('secController', function ($scope, $http, keycloak) {
 
-    $scope.greeting = {id: 'xxx', content: 'Hello World!'};
     $scope.comments= [];
-
-    $scope.greet = function() {
-        $scope.greeting.content += " more";
+    $scope.user = {
+        name: 'ahh',
+        isUser: false,
+        isAdmin: false
     };
+    $scope.isTokenExpired = keycloak.isTokenExpired;
+    $scope.token = keycloak.token;
 
     $scope.getComment = function() {
         $http.get('/api/comment').success(function (data) {
             $scope.comments = data;
         })
     };
+
+    $scope.logout = function() {
+        keycloak.logout();
+    };
+
+    keycloak.loadUserInfo().success(function (userInfo) {
+        $scope.user.name = userInfo.name;
+        $scope.$apply();
+    });
+    $scope.user.isUser = keycloak.hasRealmRole("user");
+    $scope.user.isAdmin = keycloak.hasRealmRole("admin");
+
 });
+
+// use bearer token when calling backend
+secDemoApp.config(['$httpProvider', function($httpProvider) {
+    var token = window._keycloak.token;
+    $httpProvider.defaults.headers.common['Authorization'] = 'BEARER ' + token;
+}]);
+
+
+angular.element(document).ready(function() {
+    window._keycloak = Keycloak();
+
+    secDemoApp.factory('keycloak', function($window) {
+        return $window._keycloak;
+    });
+
+    window._keycloak
+        .init({
+            onLoad: 'login-required'
+        })
+        .success(function () {
+
+            angular.bootstrap(document, ['secDemoApp']);
+        });
+
+});
+
