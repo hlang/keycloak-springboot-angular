3f8a6cf2dc4b9a44ebaa841f8b1e3d1f2d5278f3
hartmut <hartmut.lang@gmail.com>
2016-11-10 16:19:38 +0100

    keep comments sorted

--- src/main/java/de/hartmut/spring/sec/comment/CommentRepo.java
+++ src/main/java/de/hartmut/spring/sec/comment/CommentRepo.java
@@ -2,37 +2,32 @@ package de.hartmut.spring.sec.comment;
 
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/**
- * Created by hartmut on 10.11.16.
- */
 @Component
 public class CommentRepo {
-    private Map<String, Comment> commentMap;
+    private List<Comment> comments;
 
     public CommentRepo() {
-        commentMap = Stream.of("My first comment", "second comment", "another")
+        comments = Stream.of("My first comment", "second comment", "another")
                 .map(comment -> new Comment(UUID.randomUUID().toString(), comment))
-                .collect(Collectors.toMap(Comment::getId, c -> c));
+                .collect(Collectors.toList());
     }
 
     public List<Comment> getComments() {
-        return new ArrayList<>(commentMap.values());
+        return comments;
     }
 
     public Comment addComment(String commentStr) {
         Comment comment = new Comment(UUID.randomUUID().toString(), commentStr);
-        commentMap.put(comment.getId(), comment);
+        comments.add(comment);
         return comment;
     }
 
     public void delete(String id) {
-        commentMap.remove(id);
+        comments.removeIf(comment -> comment.getId().equals(id));
     }
 }
