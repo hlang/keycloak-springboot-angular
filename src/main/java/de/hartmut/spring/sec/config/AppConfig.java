a8192c8b71ff4f8e3453d13f9bedfe7e91923ffc
hartmut <hartmut.lang@gmail.com>
2016-11-11 09:20:56 +0100

    reaoad for comments added, app-configuration added

---
+++ src/main/java/de/hartmut/spring/sec/config/AppConfig.java
@@ -0,0 +1,34 @@
+package de.hartmut.spring.sec.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app")
+public class AppConfig {
+    private String kcRealm;
+    private String kcAuthServerUrl;
+    private String kcBackendSecret;
+
+    public String getKcRealm() {
+        return kcRealm;
+    }
+
+    public void setKcRealm(String kcRealm) {
+        this.kcRealm = kcRealm;
+    }
+
+    public String getKcAuthServerUrl() {
+        return kcAuthServerUrl;
+    }
+
+    public void setKcAuthServerUrl(String kcAuthServerUrl) {
+        this.kcAuthServerUrl = kcAuthServerUrl;
+    }
+
+    public String getKcBackendSecret() {
+        return kcBackendSecret;
+    }
+
+    public void setKcBackendSecret(String kcBackendSecret) {
+        this.kcBackendSecret = kcBackendSecret;
+    }
+}
